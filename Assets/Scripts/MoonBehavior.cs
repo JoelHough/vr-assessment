04d6a55a5afce05c18dccc9fa6e1cfdea28ee1a9
Cameron Sutter <cameronsutter0@gmail.com>
2015-10-13 12:53:55 -0600

    gave moon TTL

--- Assets/Scripts/MoonBehavior.cs
+++ Assets/Scripts/MoonBehavior.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class MoonBehavior : MonoBehaviour {
 
+	public float TTL = 4f;
+
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine ("TimeToLive");
 	}
 	
 	// Update is called once per frame
@@ -15,6 +17,11 @@ public class MoonBehavior : MonoBehaviour {
 
 	void OnCollisionEnter (Collision col)
 	{
+		Destroy (gameObject);
+	}
+
+	IEnumerator TimeToLive(){
+		yield return new WaitForSeconds(TTL);
 		Destroy (gameObject);
 	}
 }

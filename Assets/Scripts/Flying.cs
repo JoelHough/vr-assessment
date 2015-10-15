fde22f936bb0a8914993b7d3f8cdc253d56e4c87
Sterling Cobb <sterling@instructure.com>
2015-10-15 13:24:16 -0600

    allow user to fly around

---
+++ Assets/Scripts/Flying.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flying : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Rotate(new Vector3(0,-speed * Time.deltaTime,0));
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Rotate(new Vector3(0,speed * Time.deltaTime,0));
+		}
+
+	}
+}

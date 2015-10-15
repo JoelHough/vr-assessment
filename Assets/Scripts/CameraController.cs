68b13b6a225cfd1b25ad8dd64e6566a19f13d435
Sterling Cobb <sterling@instructure.com>
2015-10-15 16:20:10 -0600

    keep camera angles when flying

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject invisibleCameraObject;
+	private Vector3 offset;
+	
+	// Use this for initialization
+	void Start () {
+		
+		offset = transform.position - invisibleCameraObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = invisibleCameraObject.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/InvisibleObjectMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvisibleObjectMovement : MonoBehaviour {
+	
+	
+	public float speed;
+	private Rigidbody rb;
+	private Transform tf;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		tf = GetComponent<Transform> ();
+	}
+
+	
+	void FixedUpdate () 
+	{
+	    float moveHorizontal = Input.GetAxis ("Horizontal");
+	    float moveVertical = Input.GetAxis ("Vertical");
+		
+	    Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.MovePosition(transform.position + movement * Time.deltaTime * speed);
+
+	}
+}
